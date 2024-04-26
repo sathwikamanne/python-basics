aa262bd02d21bc4b68a1899638d9cd294be2ace6
M SATHWIKA <167073203+sathwikamanne@users.noreply.github.com>
2024-04-26 22:54:14 +0530

    Create hakerrank Task1 loop.py

---
+++ hakerrank Task1 loop.py
@@ -0,0 +1,15 @@
+#The provided code stub reads and integer,n, from STDIN. For all non-negative integers i<n, print i^2.
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(n):
+        print(i*i)
+
+# Input (stdin)
+#5
+#Your Output (stdout)
+#0
+#1
+#4
+#9
+#16
