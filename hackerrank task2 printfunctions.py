af0b6524f4bb91fbb30e85e78f94c443e2b8be80
M SATHWIKA <167073203+sathwikamanne@users.noreply.github.com>
2024-04-26 23:01:26 +0530

    Create hackerrank task2 printfunctions.py

---
+++ hackerrank task2 printfunctions.py
@@ -0,0 +1,10 @@
+#The included code stub will read an integer n from studing without using any string method,try to print the following 123....n
+
+if _name_ == '_main_':
+    n = int(input().strip())
+    for i in range(1,n+1):
+        print(i,end="")
+#Input (stdin)
+#3
+#Your Output (stdout)
+#123
